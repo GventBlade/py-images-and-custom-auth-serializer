2ca8111681a04ed6c6bdf85fcc4e7f8a801c3576
Vitalii Ivanchenko <vanno1234567890@gmail.com>
2025-08-10 01:07:38 +0300

    solution3

--- user/admin.py
+++ user/admin.py
@@ -6,5 +6,19 @@ class UserAdmin(BaseUserAdmin):
     list_display = ("email", "is_staff")
     search_fields = ("email",)
     ordering = ("email",)
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        ("Personal info", {"fields": ("first_name", "last_name")}),
+        ("Permissions", {"fields": ("is_active", "is_staff", "is_superuser", "groups", "user_permissions")}),
+        ("Important dates", {"fields": ("last_login", "date_joined")}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": ("email", "password", "password2"),
+        }),
+    )
+
 
 admin.site.register(User, UserAdmin)
